5867cfd02fd08adae0c3a50bbba1e6711e1b0ea0
Dipika Singh <Dipikacs10026@gmail.com>
2018-08-01 18:06:47 +0530

    new file added

---
+++ IndexJavascriptfile.js
@@ -0,0 +1,11 @@
+<script>
+/*
+The code below will change
+the heading with id = "myH"
+and the paragraph with id = "myp"
+*/
+var a;
+document.getElementById("myH").innerHTML = "JavaScript Comments";
+document.getElementById("myP").innerHTML = "My first paragraph.";
+var aa='kjdj';
+</script>
