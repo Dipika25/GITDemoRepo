748f24c916238efe6d85094aacb1548d6b61f067
Dipika Singh <Dipikacs10026@gmail.com>
2018-08-02 18:00:26 +0530

    ffff

--- Demo.Po.js
+++ Demo.Po.js
@@ -17,6 +17,25 @@ describe('angularjs homepage', function() {
 
       todoList = element.all(by.repeater('todo in todoList.todos'));
     });
+    describe('ElementFinder.then', function() {
+      var fqdn = browser.params.fqdn;
+    
+      beforeEach(function(){
+        browser.get('http://' + fqdn);
+        browser.ignoreSynchronization = true;
+      });  
+    it('should always return a promise', function(){
+        var e1 = element(by.tagName("body")).then(function(){});
+        expect(e1).toBeUndefined();
+        //expect(typeof (e1.then)).toBe("function");
+    
+        var e1 = element(by.tagName("body"))
+        .then(function(){return protractor.promise.fullyResolved({});});
+        expect(e1).not.toBeUndefined();
+        expect(typeof (e1.then)).toBe("function");
+      });
+    
+    });
 
     it('should list todos', function() {
       expect(todoList.count()).toEqual(2);
